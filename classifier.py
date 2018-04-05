00c34f61fe7e393bc89aa1a365d1ca4a0334f2ec
Bobby Ranjan <bbbranjan@gmail.com>
2018-04-06 00:43:32 +0800

    Added Word2Vec with Random Forest classification.

--- classifier.py
+++ classifier.py
@@ -1,4 +1,7 @@
+from gensim.models import word2vec
+
 from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, cohen_kappa_score, precision_recall_fscore_support
@@ -6,9 +9,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 
 import pandas as pd
+import numpy as np
 
-
-def main():
+def tfidfClassification(train_split):
 	# Load the dataframe
 	dataframe = pd.read_csv('./wine-reviews/winemag-data_labelled1500.csv')
 
@@ -23,8 +26,6 @@ def main():
 	vectorizer = TfidfVectorizer(analyzer='word', strip_accents='ascii', lowercase=True, max_features=10000)
 	features = vectorizer.fit_transform(dataframe['description'])
 	features_nd = features.toarray() # for easy usage
-
-	train_split = 0.8
 
 	X_train, X_test, y_train, y_test  = train_test_split(features_nd, data_labels, train_size=train_split)
 	binary_y_test = [1 if y == 'pos' else 0 for y in y_test]
@@ -71,6 +72,117 @@ def main():
 	print('Precision Score: {}'.format(svc_precision))
 	print('Recall Score: {}'.format(svc_recall))
 	print('F-1 Score: {}'.format(svc_fbeta_score))
+
+
+# Function to average all word vectors in a paragraph
+def featureVecMethod(words, model, num_features):
+    # Pre-initialising empty numpy array for speed
+    featureVec = np.zeros(num_features,dtype="float32")
+    nwords = 0
+    
+    #Converting Index2Word which is a list to a set for better speed in the execution.
+    index2word_set = set(model.wv.index2word)
+    
+    for word in  words:
+        if word in index2word_set:
+            nwords = nwords + 1
+            featureVec = np.add(featureVec,model[word])
+    
+    # Dividing the result by number of words to get average
+    featureVec = np.divide(featureVec, nwords)
+    return featureVec
+
+# Function for calculating the average feature vector
+def getAvgFeatureVecs(reviews, model, num_features):
+    counter = 0
+    reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+    for review in reviews:
+        reviewFeatureVecs[counter] = featureVecMethod(review, model, num_features)
+        counter = counter+1
+        
+    return reviewFeatureVecs
+
+def Word2VecClassification(train_split):
+	# Load the dataframe
+	dataframe = pd.read_csv('./wine-reviews/winemag-data_labelled1500_light.csv')
+
+	data_labels = []
+
+	for index, row in dataframe.iterrows():
+		if row['points'] >= 85:
+			data_labels.append('pos')
+		else:
+			data_labels.append('neg')
+
+	# Creating the model and setting values for the various parameters
+	num_features = 300  # Word vector dimensionality
+	min_word_count = 40 # Minimum word count
+	num_workers = 4     # Number of parallel threads
+	context = 10        # Context window size
+	downsampling = 1e-3 # (0.001) Downsample setting for frequent words
+
+	# Initializing the train model
+	
+	model = word2vec.Word2Vec(dataframe['description'],\
+	                          workers=num_workers,\
+	                          size=num_features,\
+	                          min_count=min_word_count,\
+	                          window=context,
+	                          sample=downsampling)
+
+	# To make the model memory efficient
+	model.init_sims(replace=True)
+
+	# Saving the model for later use. Can be loaded using Word2Vec.load()
+	model_name = "labelled1500_light_300features_40minwords_10context"
+	model.save(model_name)
+
+
+	train_reviews = dataframe.sample(frac=train_split)
+	train_data_labels = []
+	for index, row in train_reviews.iterrows():
+		train_data_labels.append(data_labels[index])
+	trainDataVecs = getAvgFeatureVecs(train_reviews['description'], model, num_features)
+
+	test_reviews = dataframe.sample(frac=(1-train_split))
+	test_data_labels = []
+
+	for index, row in test_reviews.iterrows():
+		if data_labels[index] == 'pos':
+			test_data_labels.append(1)
+		else:
+			test_data_labels.append(0)
+	testDataVecs = getAvgFeatureVecs(test_reviews['description'], model, num_features)
+
+	# Random Forest method
+	print('\nRandom Forest Method')
+	print('===========================')
+	forest = RandomForestClassifier(n_estimators = 100)
+	forest = forest.fit(trainDataVecs, train_data_labels)
+	result = forest.predict(testDataVecs)
+
+	predicted_labels = []
+	for label in result:
+		if label == 'pos':
+			predicted_labels.append(1)
+		else:
+			predicted_labels.append(0)
+
+	print('Accuracy Score: {}'.format(accuracy_score(test_data_labels, predicted_labels)))
+	print('Cohen Kappa Score: {}'.format(cohen_kappa_score(test_data_labels, predicted_labels)))
+	rf_precision, rf_recall, rf_fbeta_score, support = precision_recall_fscore_support(test_data_labels, predicted_labels, average='binary')
+	print('Precision Score: {}'.format(rf_precision))
+	print('Recall Score: {}'.format(rf_recall))
+	print('F-1 Score: {}'.format(rf_fbeta_score))
+
+def main():
+	"""
+	Main method.
+	"""
+	train_split = 0.8
+	tfidfClassification(train_split)
+	Word2VecClassification(train_split)
+
 
 
 
